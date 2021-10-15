66fda62db32885908554d4fe712deceabb688838
unikittydev <777ruspacan777@gmail.com>
2021-10-15 10:43:22 +0300

    Alternative Audio Manager Created

---
+++ Dream Logic/Assets/Scripts/Core/Audio/AudioClipSettings.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class AudioClipSettings : ScriptableObject
+    {
+        public AudioClip clip;
+        public float maxVolume;
+        public bool loop;
+    }
+}
+
